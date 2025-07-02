68e5c1732b08e704e15eb09bc9ee12174c503309
Dyl-DJM <dyl.dejesusmilitar@gmail.com>
2025-07-02 17:13:37 +0200

    feat: remove button for current move

--- src/App.js
+++ src/App.js
@@ -42,25 +42,6 @@ function Board({ xIsNext, squares, onPlay }) {
     <div className="status">{status}</div>
     {board}
     </>
-    // <>
-    //   <div className="status">{status}</div>
-    //   <div className="board-row">
-    //     <Square value={squares[0]} onSquareClick={() => handleClick(0)}/>
-    //     <Square value={squares[1]} onSquareClick={() => handleClick(1)}/>
-    //     <Square value={squares[2]} onSquareClick={() => handleClick(2)}/>
-    //   </div>
-    //   <div className="board-row">
-    //     <Square value={squares[3]} onSquareClick={() => handleClick(3)}/>
-    //     <Square value={squares[4]} onSquareClick={() => handleClick(4)}/>
-    //     <Square value={squares[5]} onSquareClick={() => handleClick(5)}/>
-    //   </div>
-    //   <div className="board-row">
-    //     <Square value={squares[6]} onSquareClick={() => handleClick(6)}/>
-    //     <Square value={squares[7]} onSquareClick={() => handleClick(7)}/>
-    //     <Square value={squares[8]} onSquareClick={() => handleClick(8)}/>
-
-    //   </div>
-    // </>
   );
 }
 
@@ -83,6 +64,9 @@ export default function Game() {
 
   const moves = history.map((squares, move) => {
     let description;
+    if(move === history.length - 1) {
+      return <div>Vous êtes au coup {move}</div>;
+    }
     if (move > 0){
       description = "Aller au coup #" + move;
     } else {
